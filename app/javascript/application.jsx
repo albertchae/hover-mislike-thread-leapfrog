a95f20001dea5968e0d43d9913b0478c64698d9d
Albert Chae <albertchae@users.noreply.github.com>
2023-02-01 01:55:43 -0800

    Add a React controlled form for submitting the question.
    Does not actually submit question yet, just handles managing state with
    hooks

--- app/javascript/application.jsx
+++ app/javascript/application.jsx
@@ -1,15 +1,35 @@
 // Entry point for the build script in your package.json
 
-import React from 'react'
-import { createRoot } from 'react-dom/client';
+import React, { useState } from "react";
+import { createRoot } from "react-dom/client";
 
 const App = () => {
-  return (<div>Hello, Rails 7 from Rails!</div>)
-}
+  const [question, setQuestion] = useState("");
+  function handleChange(event) {
+    setQuestion(event.target.value);
+  }
+  function handleSubmit(event) {
+    alert("A question was submitted: " + question);
+    event.preventDefault();
+  }
 
-document.addEventListener('DOMContentLoaded', () => {
-  const container = document.getElementById('app');
-  const root = createRoot(container); // createRoot(container!) if you use TypeScript
+  return (
+    <div className="main">
+      <p className="credits">
+        This is an experiment in using AI to make my book's content more
+        accessible. Ask a question and AI'll answer it in real-time:
+      </p>
+
+      <form onSubmit={handleSubmit}>
+        <textarea value={question} onChange={handleChange} />{" "}
+        <input type="submit" value="Ask question" />
+      </form>
+    </div>
+  );
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.getElementById("app");
+  const root = createRoot(container);
   root.render(<App />);
-})
-
+});
