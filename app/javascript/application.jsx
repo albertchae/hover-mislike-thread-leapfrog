dadb364ddf945d1b6d01b1b660de3078dff6d1b2
Albert Chae <albertchae@users.noreply.github.com>
2023-02-01 01:41:56 -0800

    Switch to React 18 createRoot
    Followed steps here
    https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#updates-to-client-rendering-apis

--- app/javascript/application.jsx
+++ app/javascript/application.jsx
@@ -1,14 +1,15 @@
 // Entry point for the build script in your package.json
 
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
+import React from 'react'
+import { createRoot } from 'react-dom/client';
 
 const App = () => {
   return (<div>Hello, Rails 7 from Rails!</div>)
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  const rootEl = document.getElementById('app')
-  ReactDOM.render(<App />, rootEl)
+  const container = document.getElementById('app');
+  const root = createRoot(container); // createRoot(container!) if you use TypeScript
+  root.render(<App />);
 })
 
