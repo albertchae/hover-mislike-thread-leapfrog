8266dcca3da42522c92ab2ce24218c214e194454
Albert Chae <albertchae@users.noreply.github.com>
2023-02-01 17:39:59 -0800

    Listed out steps needed to get answer from OpenAI based on my understanding of how the python version of askmybook is working

--- app/services/answer_generator.rb
+++ app/services/answer_generator.rb
@@ -1,6 +1,13 @@
 class AnswerGenerator
   def answer_question(question)
-
+    # 1. add ? if needed
+    # 2. look up question in cache, return cached answer if it exists
+    # 3. load pregenerated embeddings to use in constructing prompt
+    # 4. compute similarity of question with book text to get context
+    # 5. construct prompt using context + hardcoded questions and answers
+    # 6. send prompt to completions API for answer
+    # 7. cache question and answer
+    # 8 return answer
     "You asked me #{question}"
   end
 
