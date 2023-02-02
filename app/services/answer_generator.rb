ec5a9d37dd12c19d3149b781fac61cebc8e2a52b
Albert Chae <albertchae@users.noreply.github.com>
2023-02-01 17:59:46 -0800

    Actually send question to OpenAI completions API
    - Using the same temperature, max_tokens and model parameters as the
      python codebase.
    - Parses the answer out of the repsonse the same way (ruby strip takes
      care of newline so no argument is needed)
    
    The prompt is just the question verbatim for now

--- app/services/answer_generator.rb
+++ app/services/answer_generator.rb
@@ -2,15 +2,29 @@ class AnswerGenerator
   def answer_question(question)
     # 1. add ? if needed
     question = add_question_mark_if_necessary(question)
-    
+
     # 2. look up question in cache, return cached answer if it exists
     # 3. load pregenerated embeddings to use in constructing prompt
     # 4. compute similarity of question with book text to get context
     # 5. construct prompt using context + hardcoded questions and answers
+    prompt = construct_prompt(question)
     # 6. send prompt to completions API for answer
+    response = OpenAI::Client.new.completions(
+      parameters: {
+        prompt: prompt,
+        # We use temperature of 0.0 because it gives the most predictable, factual answer.
+        temperature: 0.0,
+        max_tokens: 150,
+        model: "text-davinci-003"
+      })
+    answer = response["choices"][0]["text"].strip
     # 7. cache question and answer
     # 8 return answer
-    "You asked me #{question}"
+    answer
+  end
+
+  def construct_prompt(question)
+    question
   end
 
   private
