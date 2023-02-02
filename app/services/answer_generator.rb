909274f74667e954e064d29264ccab5aba8ffa90
Albert Chae <albertchae@users.noreply.github.com>
2023-02-01 17:45:06 -0800

    Add ? to question if not already there

--- app/services/answer_generator.rb
+++ app/services/answer_generator.rb
@@ -1,6 +1,8 @@
 class AnswerGenerator
   def answer_question(question)
     # 1. add ? if needed
+    question = add_question_mark_if_necessary(question)
+    
     # 2. look up question in cache, return cached answer if it exists
     # 3. load pregenerated embeddings to use in constructing prompt
     # 4. compute similarity of question with book text to get context
@@ -11,5 +13,11 @@ class AnswerGenerator
     "You asked me #{question}"
   end
 
+  private
+
+  def add_question_mark_if_necessary(question)
+    return question if question.ends_with?('?')
+    question + '?'
+  end
 
 end
