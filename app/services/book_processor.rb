a60a9dd85c3c9694cbfc09c426f378ada53d1cd0
Albert Chae <albertchae@users.noreply.github.com>
2023-02-05 22:55:56 -0800

    Script to read pdf and output pages.csv
    - titles pages with same format as python codebase, e.g. Page 1, Page 2
    - extracts page text using pdf-reader gem. This gem appears to be worse
      than PyPDF2 at extracting text and matching it to the correct page, so
      I tried installing an alternative (poppler) but was having enough
      compilation issues that I decided to stick to this for now.
    - counts tokens using same model as python codebase, i.e. "gpt2"

--- app/services/book_processor.rb
+++ app/services/book_processor.rb
@@ -1,9 +1,29 @@
 class BookProcessor
   def initialize(pdf_path)
     @pdf_path = pdf_path
+    @tokenizer = Tokenizers.from_pretrained("gpt2")
   end
   def call
-    puts @pdf_path
+    book = PDF::Reader.new(@pdf_path)
+
+    # Parse book contents into dataframe
+    pages = book.pages.map do |page|
+      # Replace newlines with spaces and replace multiple spaces with a single space
+      page_text_with_compressed_whitespace = page.text.gsub("\n", " ").gsub(/\s+/, " ")
+      {
+        title: "Page #{page.number}",
+        content: page_text_with_compressed_whitespace,
+        tokens: @tokenizer.encode(page_text_with_compressed_whitespace).tokens.count
+      }
+    end
+    book_pages_df = Rover::DataFrame.new(pages)
+
+    # Write book content CSV for use by askmybook application
+    File.write("#{@pdf_path}.pages.csv", book_pages_df.to_csv)
+
+
+
+
 
   end
 end
--- lib/tasks/process_book.rake
+++ lib/tasks/process_book.rake
@@ -1,3 +1,4 @@
+# Example usage: bin/rake process_book -- --pdf lecture1.pdf
 task :process_book => [:environment] do |_t, _args|
   # boilerplate for parsing arguments https://www.seancdavis.com/posts/4-ways-to-pass-arguments-to-a-rake-task/
   options = {}
