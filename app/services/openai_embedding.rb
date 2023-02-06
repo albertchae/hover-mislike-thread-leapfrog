7832597d6e3dec4739f1e26cd9dfdd3ac0d1cc2b
Albert Chae <albertchae@users.noreply.github.com>
2023-02-05 23:30:21 -0800

    Rename arguments in embedding client to be more generic for doc and query

--- app/services/openai_embedding.rb
+++ app/services/openai_embedding.rb
@@ -2,8 +2,8 @@ class OpenaiEmbedding
   DOC_EMBEDDINGS_MODEL = "text-search-curie-doc-001"
   QUERY_EMBEDDINGS_MODEL = "text-search-curie-query-001"
 
-  def get_doc_embedding(question)
-    get_embedding(question, DOC_EMBEDDINGS_MODEL)
+  def get_doc_embedding(text)
+    get_embedding(text, DOC_EMBEDDINGS_MODEL)
   end
 
   def get_query_embedding(question)
@@ -11,11 +11,11 @@ class OpenaiEmbedding
   end
 
   # Get an embedding vector from OpenAI API
-  def get_embedding(question, model)
+  def get_embedding(text, model)
     response = OpenAI::Client.new.embeddings(
       parameters: {
         model: model,
-        input: question
+        input: text
       }
     )
     # this is a 4096 element array, is this always true?
